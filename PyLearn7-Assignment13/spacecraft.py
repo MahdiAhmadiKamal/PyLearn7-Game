41bfbda93aa96ca30cf2e8f1eb59039ec18ece7a
Mahdi Ahmadi Kamal <909stars@gmail.com>
2023-12-18 23:39:43 +0330

    2nd commit

--- PyLearn7-Assignment13/spacecraft.py
+++ PyLearn7-Assignment13/spacecraft.py
@@ -9,16 +9,16 @@ class Enemy:
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__()
+        super().__init__(width=1280, height=700, title="Spacecraft Game")
         arcade.set_background_color(arcade.color.DARK_BLUE)
-        self.background = arcade.load_texture("session 13\camera.jpg")
+        self.background = arcade.load_texture(":resources:images/backgrounds/stars.png")
 
     # methods
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_lrwh_rectangle_textured(10,20,500, 500, self.background)
+        arcade.draw_lrwh_rectangle_textured(0,0,1280, 700, self.background)
         
-
 
 window = Game()
 arcade.run()
+
