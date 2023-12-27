e785b22f587a9be9479c3f0154ff08ee5ff78caf
Mahdi Ahmadi Kamal <909stars@gmail.com>
2023-12-27 20:30:30 +0330

    last commit

--- PyLearn7-Assignment13/spacecraft.py
+++ PyLearn7-Assignment13/spacecraft.py
@@ -4,9 +4,9 @@ import arcade
 
 class Spacecraft(arcade.Sprite):
     def __init__(self, w):
-        super().__init__(":resources:images/space_shooter/playerShip1_green.png")
+        super().__init__("D:\PyLearn7\Assignments\PyLearn7-Game\PyLearn7-Assignment13\my_spacecraft.png")
         self.center_x = w//2
         self.center_y= 80
-        self.width = 64
-        self.height = 48
+        self.width = 120
+        self.height = 120
         self.speed = 8
