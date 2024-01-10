af5dd580a78d43471acda29328c605a1d35cf5ff
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-01-10 20:15:16 +0330

    5th commit

--- PyLearn7-Assignment14/spacecraft.py
+++ PyLearn7-Assignment14/spacecraft.py
@@ -7,12 +7,23 @@ class Spacecraft(arcade.Sprite):
         super().__init__("D:\PyLearn7\Assignments\PyLearn7-Game\PyLearn7-Assignment13\my_spacecraft.png")
         self.center_x = w//2
         self.center_y= 80
+        self.change_x = 0
+        self.change_y = 0
         self.width = 100
         self.height = 100
-        self.speed = 8
+        self.speed = 4
+        self.game_width = w
 
-    def move(self, direction):
-        if direction == "L":
-            self.center_x = self.center_x - self.speed
-        elif direction == "R":
-            self.center_x = self.center_x + self.speed
+    # def move(self, direction):
+    #     if direction == "L":
+    #         self.center_x = self.center_x - self.speed
+    #     elif direction == "R":
+    #         self.center_x = self.center_x + self.speed
+
+    def move(self):
+        if self.change_x == -1:
+            if self.center_x > 0:
+                self.center_x = self.center_x - self.speed      # or self.center_x -= self.speed
+        elif self.change_x == 1:
+            if self.center_x < self.game_width:
+                self.center_x = self.center_x + self.speed      # or self.center_x += self.speed
--- PyLearn7-Assignment14/starwars.py
+++ PyLearn7-Assignment14/starwars.py
@@ -7,11 +7,12 @@ class Spacecraft(arcade.Sprite):
         super().__init__("D:\PyLearn7\Assignments\PyLearn7-Game\PyLearn7-Assignment13\my_spacecraft.png")
         self.center_x = w//2
         self.center_y= 80
-        self.change_x = -1
+        self.change_x = 0
         self.change_y = 0
         self.width = 100
         self.height = 100
         self.speed = 4
+        self.game_width = w
 
     # def move(self, direction):
     #     if direction == "L":
@@ -21,9 +22,11 @@ class Spacecraft(arcade.Sprite):
 
     def move(self):
         if self.change_x == -1:
-            self.center_x = self.center_x - self.speed
+            if self.center_x > 0:
+                self.center_x = self.center_x - self.speed      # or self.center_x -= self.speed
         elif self.change_x == 1:
-            self.center_x = self.center_x + self.speed
+            if self.center_x < self.game_width:
+                self.center_x = self.center_x + self.speed      # or self.center_x += self.speed
 
 class Enemy(arcade.Sprite):
     def __init__(self, w, h):
@@ -61,10 +64,12 @@ class Game(arcade.Window):
         if symbol==arcade.key.A or symbol==arcade.key.LEFT:    #left direction
             # self.me.move("L")
             self.me.change_x = -1
-        if symbol==arcade.key.D or symbol==arcade.key.RIGHT:   #right direction
+        elif symbol==arcade.key.D or symbol==arcade.key.RIGHT:   #right direction
             # self.me.move("R")
             self.me.change_x = 1
-        if symbol==arcade.key.SPACE:
+        elif symbol==arcade.key.S or symbol==arcade.key.DOWN:   #stop
+            self.me.change_x = 0
+        elif symbol==arcade.key.SPACE:
             ...
 
     def on_update(self, delta_time: float):
