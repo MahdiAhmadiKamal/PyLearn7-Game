0ff1fd5972b566160346e14cbcebdcb6290046b8
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-01-12 23:32:26 +0330

    10th commit

--- PyLearn7-Assignment14/starwars.py
+++ PyLearn7-Assignment14/starwars.py
@@ -1,4 +1,5 @@
 import random
+import time
 import arcade
 
 
@@ -64,17 +65,20 @@ class Game(arcade.Window):
         arcade.set_background_color(arcade.color.DARK_BLUE)
         self.background = arcade.load_texture(":resources:images/backgrounds/stars.png")
         self.me = Spacecraft(self.width)
-        # self.enem = Enemy(self.width, self.height)
-        self.enems = []
+        # self.enemy = Enemy(self.width, self.height)
+        self.enemies = []
+        self.start_time = time.time()
+        self.enemy_gap_time = 3
+
 
     # methods
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0,0,self.width, self.height, self.background)
         self.me.draw()
-        # self.enem.draw()
-        for enem in self.enems:
-            enem.draw()
+        # self.enemy.draw()
+        for enemy in self.enemies:
+            enemy.draw()
 
         for bullet in self.me.bullet_list:
             bullet.draw()
@@ -96,33 +100,39 @@ class Game(arcade.Window):
         self.me.change_x = 0   
         
     def on_update(self, delta_time: float):
-        for enem in self.enems:
-            if arcade.check_for_collision(self.me, enem):
+        self.end_time = time.time()
+        for enemy in self.enemies:
+            if arcade.check_for_collision(self.me, enemy):
                 print ("Game Over!")
                 exit(0)
 
-        for enem in self.enems:
+        for enemy in self.enemies:
             for bullet in self.me.bullet_list:
-                if arcade.check_for_collision(enem, bullet):
-                    self.enems.remove(enem)
+                if arcade.check_for_collision(enemy, bullet):
+                    self.enemies.remove(enemy)
                     self.me.bullet_list.remove(bullet)
 
-        # self.enem.move()
+        # self.enemy.move()
         self.me.move()
 
-        for enem in self.enems:
-            enem.move()
+        for enemy in self.enemies:
+            enemy.move()
 
         for bullet in self.me.bullet_list:
             bullet.move()
 
-        for enem in self.enems:
-            if enem.center_y < 0:
-               self.enems.remove(enem)  
-               
-        if random.randint(1, 80)==6:   
-            self.new_enem = Enemy(self.width, self.height)
-            self.enems.append(self.new_enem)
+        for enemy in self.enemies:
+            if enemy.center_y < 0:
+               self.enemies.remove(enemy)  
+        
+        if (self.end_time-self.start_time) >= self.enemy_gap_time:
+            self.new_enemy = Enemy(self.width, self.height)
+            self.enemies.append(self.new_enemy)
+            self.start_time=time.time()
+        
+        # if random.randint(1, 80)==6:   
+        #     self.new_enemy = Enemy(self.width, self.height)
+        #     self.enemies.append(self.new_enemy)
 
 window = Game()
 arcade.run()
