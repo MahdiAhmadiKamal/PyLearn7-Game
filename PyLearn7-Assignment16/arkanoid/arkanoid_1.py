3db56c938fb186cf4b650f3d4d46165dcfdf019f
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-01-25 11:14:21 +0330

    3rd commit

--- PyLearn7-Assignment16/arkanoid/arkanoid_1.py
+++ PyLearn7-Assignment16/arkanoid/arkanoid_1.py
@@ -5,8 +5,9 @@ class Ball(arcade.Sprite):
         super().__init__("pics\klipartz.com.png")
         self.center_x = player.center_x
         self.center_y = player.center_y + player.height
-        self.width = 25
-        self.height = 25
+        self.radius = 12.5
+        self.width = 2 * self.radius
+        self.height = 2 * self.radius
         self.change_x = 0
         self.change_y = 0
         self.speed = 5
@@ -24,13 +25,14 @@ class Block(arcade.Sprite):
         self.color = c
         self.width = 60
         self.height = 30
-        self.row = []
+        # self.row = []
 
 
     def draw(self):
-        for i in range (7):
-            b = arcade.draw_rectangle_filled(self.center_x+63*i, self.center_y, self.width, self.height, self.color)
-            self.row.append(b)
+        # for i in range (7):
+        #     arcade.draw_rectangle_filled(self.center_x+63*i, self.center_y, self.width, self.height, self.color)
+        
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
 
 class Vaus(arcade.Sprite):
     def __init__(self, game):
@@ -56,16 +58,23 @@ class Game(arcade.Window):
         self.background = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
         self.player = Vaus(self)
         self.ball = Ball(self.player)
-        # for i in range (5):
-        self.block = Block(35, self.height//2, arcade.color.RED_BROWN)
+        self.block_row = []
+        for j in range (4):
+            for i in range (8):
+                if j%2==0:
+                    self.block = Block(35+61*i, self.height//2 + 35*j, arcade.color.RED_BROWN)
+                    self.block_row.append(self.block)
+                elif j%2==1:
+                    self.block = Block(35+61*i, self.height//2 + 35*j, arcade.color.RED_ORANGE)
+                    self.block_row.append(self.block)
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
         self.player.draw()
         self.ball.draw()
-        
-        self.block.draw()
+        for self.block in self.block_row:
+            self.block.draw()
         arcade.finish_render()
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
@@ -76,22 +85,32 @@ class Game(arcade.Window):
             self.player.center_x = x
             self.ball.center_x = x
 
-
     def on_update(self, delta_time: float):
         self.ball.move()
-        for b in range (len(self.block.row)-1):
-            if arcade.check_for_collision(self.ball, self.block.row[b]):
-                del self.block.row[b]
+        if arcade.check_for_collision(self.ball, self.player):
+            self.ball.change_y *= -1
+        
+        if self.ball.center_x < self.ball.radius or self.ball.center_x > self.width - self.ball.radius:
+            self.ball.change_x *= -1
+
+        if self.ball.center_y > self.height - self.ball.radius:
+            self.ball.change_y *= -1
+
+
+        for i in range (len(self.block_row)):
+            try:
+                if arcade.check_for_collision(self.ball, self.block_row[i]):
+                    self.ball.change_y *= -1
+                    del self.block_row[i]
+            except(IndexError):
+                pass
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            self.ball.change_x = 0.25
+            self.ball.change_x = 0.15
             self.ball.change_y = 1
             
             
-            
-
-
 
 game = Game()
 arcade.run()
