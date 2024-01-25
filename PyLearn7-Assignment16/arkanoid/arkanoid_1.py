23e3279ffb64c2f856197b62d5d759a9e1a3c1fb
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-01-25 19:21:24 +0330

    4th commit

--- PyLearn7-Assignment16/arkanoid/arkanoid_1.py
+++ PyLearn7-Assignment16/arkanoid/arkanoid_1.py
@@ -1,3 +1,4 @@
+import random
 import arcade
 
 class Ball(arcade.Sprite):
@@ -10,7 +11,7 @@ class Ball(arcade.Sprite):
         self.height = 2 * self.radius
         self.change_x = 0
         self.change_y = 0
-        self.speed = 5
+        self.speed = 6
 
     def move(self):
         self.center_x += self.change_x * self.speed
@@ -27,10 +28,7 @@ class Block(arcade.Sprite):
         self.height = 30
         # self.row = []
 
-
     def draw(self):
-        # for i in range (7):
-        #     arcade.draw_rectangle_filled(self.center_x+63*i, self.center_y, self.width, self.height, self.color)
         
         arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
 
@@ -39,13 +37,34 @@ class Vaus(arcade.Sprite):
         super().__init__()
         self.center_x = game.width//2
         self.center_y = 40
-        self.speed = 4
+        self.speed = 6
         self.change_x = 0
         self.change_y = 0
         self.color = arcade.color.GREEN
-        self.width = 80
+        self.width = 90
         self.height = 22
         self.score = 0
+
+    def move(self):                        # for moving the Vaus using the keyboard
+        if game.ball.center_y > self.center_y + self.height + game.ball.radius: 
+            # when the ball is not on the Vaus
+            if self.change_x == -1:
+                if self.center_x - self.width//2 > 0:
+                    self.center_x = self.center_x - self.speed
+            elif self.change_x == 1:
+                if self.center_x + self.width//2 < game.width:
+                    self.center_x = self.center_x + self.speed 
+
+        else:                             
+            # when the ball is on the Vaus
+            if self.change_x == -1:
+                if self.center_x - self.width//2 > 0:
+                    self.center_x = self.center_x - self.speed
+                    game.ball.center_x = game.ball.center_x - self.speed
+            elif self.change_x == 1:
+                if self.center_x + self.width//2 < game.width:
+                    self.center_x = self.center_x + self.speed
+                    game.ball.center_x = game.ball.center_x + self.speed 
 
     def draw(self):
         arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
@@ -59,6 +78,9 @@ class Game(arcade.Window):
         self.player = Vaus(self)
         self.ball = Ball(self.player)
         self.block_row = []
+        self.condition = ""
+        self.count = 0
+
         for j in range (4):
             for i in range (8):
                 if j%2==0:
@@ -71,22 +93,40 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
+        arcade.draw_text(f"score: {self.player.score}", 20, self.height-50, arcade.color.WHITE,
+                          25, font_name=("broadway"))
         self.player.draw()
         self.ball.draw()
         for self.block in self.block_row:
             self.block.draw()
+
+        if self.condition == "Game Over":
+            arcade.start_render()
+            # arcade.set_background_color(arcade.color.BLACK)
+            arcade.draw_text("Game Over",self.width/5, self.height/2, arcade.color.WHITE, 45)
         arcade.finish_render()
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
-        if self.ball.center_y > self.player.center_y + self.player.height and \
+        if self.ball.center_y > self.player.center_y + self.player.height//2 + self.ball.radius and \
             self.player.width//2 < x < self.width - self.player.width//2:
             self.player.center_x = x
         elif self.player.width//2 < x < self.width - self.player.width//2:
             self.player.center_x = x
             self.ball.center_x = x
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        
+        if symbol==arcade.key.A:        #left direction
+            self.player.change_x = -1
+        elif symbol==arcade.key.D:      #right direction
+            self.player.change_x = 1
+            
+    def on_key_release(self, symbol: int, modifiers: int):
+        self.player.change_x = 0  
+
     def on_update(self, delta_time: float):
         self.ball.move()
+        self.player.move()                  # for moving the Vaus using the keyboard
         if arcade.check_for_collision(self.ball, self.player):
             self.ball.change_y *= -1
         
@@ -96,19 +136,30 @@ class Game(arcade.Window):
         if self.ball.center_y > self.height - self.ball.radius:
             self.ball.change_y *= -1
 
+        if self.count == 3:
+            self.condition = "Game Over"
+        elif self.ball.center_y + 10*self.ball.radius < 0:
+            self.player.score -= 1
+            del self.ball
+            self.ball = Ball(self.player)
+            self.count += 1
+    
 
         for i in range (len(self.block_row)):
             try:
                 if arcade.check_for_collision(self.ball, self.block_row[i]):
                     self.ball.change_y *= -1
                     del self.block_row[i]
+                    self.player.score += 1
             except(IndexError):
                 pass
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            self.ball.change_x = 0.15
+            self.ball.change_x = 0.25
             self.ball.change_y = 1
+
+        
             
             
 
